3a0b79460b6e2e559cb24c851cc6cd9524248efc
Kayron Bragança <kayron.juan123@gmail.com>
2025-10-01 07:21:47 -0300

    Adding Repository Interface on Domain - Starting Application Developtment

---
+++ OrderAPI.Application/Interfaces/IOrderService.cs
@@ -0,0 +1,18 @@
+﻿using OrderAPI.Domain.Entities;
+using OrderAPI.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderAPI.Application.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<Order> GetOrderByIdAsync(int id);
+        Task<List<Order>> GetAllOrdersAsync();
+        Task AddOccurrence(EOccurrenceType type, DateTime dateTime);
+        Task DeleteOccurrence(int occurrenceId);
+    }
+}
---
+++ OrderAPI.Domain/Interfaces/IOrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using OrderAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderAPI.Domain.Interfaces
+{
+    public interface IOrderRepository
+    {
+        Task<Order> GetByIdAsync(int id);
+        Task<Order> GetAllAsync(int id);
+        Task AddAsync(Order order);
+        Task UpdateAsync(Order order);
+    }
+}
